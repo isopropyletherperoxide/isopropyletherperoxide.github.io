790a1c21ebb03a95b7cc21203f9e141f40a64673
Aryl Halide <endless.sine@gmail.com>
2023-05-07 00:46:14 +0300

    nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = [ pkgs.buildPackages.jekyll ];
+    nativeBuildInputs = [ pkgs.buildPackages.jekyll pkgs.buildPackages.rubyPackages.jekyll-sitemap ];
 }
