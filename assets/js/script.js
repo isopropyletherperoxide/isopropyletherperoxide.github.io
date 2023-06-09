e2b80abb0fc5ce899ad47884d43dfbc5ba86ecf6
aryl <endless.sine@gmail.com>
2023-06-10 00:00:31 +0300

    chromium fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,11 @@ function updateText() {
         // ctx.font="45px Goldman Sans Condensed Regular" 
         ctx.font="45px Goldman Sans Condensed, Noto Sans, sans-serif" 
         ctx.clearRect(0, 0, can.width, can.height);
-        ctx.drawImage(img, 0, -100, 512, 256);
+        ctx.drawImage(img, 0, 0, 512, 256);
         let lineheight = 45
         var lines = text.split('\n');
         for (var i = 0; i < lines.length; i++) {
-                ctx.fillText(lines[i], 160, 80 + (i * lineheight))
+                ctx.fillText(lines[i], 160, 55 + (i * lineheight))
         }
 }
 
