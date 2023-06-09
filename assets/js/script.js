70975280f1fd92e16b495451805b2e91ff2a6792
aryl <endless.sine@gmail.com>
2023-06-09 23:52:16 +0300

    germany generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ function updateText() {
         // ctx.font="45px Goldman Sans Condensed Regular" 
         ctx.font="45px Goldman Sans Condensed, Noto Sans, sans-serif" 
         ctx.clearRect(0, 0, can.width, can.height);
-        ctx.drawImage(img, 0, -100, 512, 512);
+        ctx.drawImage(img, 0, -100, 512, 256);
         let lineheight = 45
         var lines = text.split('\n');
         for (var i = 0; i < lines.length; i++) {
