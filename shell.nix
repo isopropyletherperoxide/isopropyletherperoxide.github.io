14175c94923bbdd785c1819b77cfb21f2699c979
aryl <endless.sine@gmail.com>
2026-04-28 12:00:23 +0300

    update section

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = [ pkgs.buildPackages.jekyll pkgs.buildPackages.rubyPackages.jekyll-sitemap ];
+    nativeBuildInputs = [ pkgs.buildPackages.jekyll pkgs.buildPackages.rubyPackages.jekyll-sitemap pkgs.buildPackages.rubyPackages.jekyll-feed ];
     shellHook = '' 
      jekyll build 
     '';
